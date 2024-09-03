9c17724628a829105c35ba28c87cdbd8775cc35f
Nubchulubs12 <nick.kuczka@gmail.com>
2024-09-02 20:31:46 -0500

    java fun

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
 Scanner input = new Scanner(System.in);
+Double radius = null;
+        while (radius == null || radius < 0) {
+            System.out.println("Enter the radius:");
 
-System.out.println("Enter in a radius: ");
+            if (input.hasNextDouble()) {
+                radius = input.nextDouble();
 
-double radius = input.nextDouble();
+            } else  {
+                System.out.println("Error: Radius must be a positive number.");
+
+            }
+        }
+
 
 double area = circle.getArea(radius);
 
